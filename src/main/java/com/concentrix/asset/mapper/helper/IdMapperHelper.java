b2714b13077f9aff6e5e40d725098a0d1cb48b12
Thinh_GJaa <xthinh4052002@gmail.com>
2025-08-15 13:32:11 +0700

    Cập nhật thành mapper chung cho các Id mapper

---
+++ src/main/java/com/concentrix/asset/mapper/helper/IdMapperHelper.java
@@ -0,0 +1,48 @@
+package com.concentrix.asset.mapper.helper;
+
+import com.concentrix.asset.entity.Account;
+import com.concentrix.asset.entity.Site;
+import com.concentrix.asset.entity.User;
+import com.concentrix.asset.exception.CustomException;
+import com.concentrix.asset.exception.ErrorCode;
+import com.concentrix.asset.repository.AccountRepository;
+import com.concentrix.asset.repository.SiteRepository;
+import com.concentrix.asset.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.mapstruct.Named;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@FieldDefaults(level = lombok.AccessLevel.PRIVATE, makeFinal = true)
+public class IdMapperHelper {
+
+    AccountRepository accountRepository;
+    SiteRepository siteRepository;
+    UserRepository userRepository;
+
+    @Named("siteIdToSite")
+    public Site siteIdToSite(Integer siteId) {
+        return siteRepository.findById(siteId)
+                .orElseThrow(() -> new CustomException(ErrorCode.SITE_NOT_FOUND, siteId));
+    }
+
+    @Named("accountIdToAccount")
+    public Account getAccountById(Integer accountId) {
+        if (accountId == null) {
+            return null;
+        }
+        return accountRepository.findById(accountId)
+                .orElseThrow(() -> new CustomException(ErrorCode.ACCOUNT_NOT_FOUND, accountId));
+    }
+
+    @Named("userEidToUser")
+    public User userEidToUser(String userEid) {
+        if (userEid == null || userEid.isBlank())
+            return null;
+
+        return userRepository.findById(userEid)
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND, userEid));
+    }
+}
