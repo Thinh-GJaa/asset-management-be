4378238901abb9ff9f76ccdc6c478b70c3a0de09
Thinh_GJaa <xthinh4052002@gmail.com>
2025-08-22 17:01:44 +0700

    Add mapper function between UserImportRequest and User Add new property mappings

--- src/main/java/com/concentrix/asset/mapper/UserMapper.java
+++ src/main/java/com/concentrix/asset/mapper/UserMapper.java
@@ -2,20 +2,34 @@ package com.concentrix.asset.mapper;
 
 import com.concentrix.asset.dto.request.CreateUserRequest;
 import com.concentrix.asset.dto.request.UpdateUserRequest;
+import com.concentrix.asset.dto.request.UserImportRequest;
 import com.concentrix.asset.dto.response.UserResponse;
 import com.concentrix.asset.entity.User;
+import com.concentrix.asset.mapper.helper.IdMapperHelper;
 import com.concentrix.asset.mapper.helper.PasswordHelper;
 import org.mapstruct.*;
 
-@Mapper(componentModel = "spring", uses = PasswordHelper.class)
+@Mapper(componentModel = "spring", uses ={PasswordHelper.class, IdMapperHelper.class})
 public interface UserMapper {
 
+    @Mapping(target = "account", source = "account")
+    @Mapping(target = "site", source = "site")
     UserResponse toUserResponse(User user);
 
     @Mapping(target = "role", defaultValue = "OTHER")
-    @Mapping(target = "password", source = "password", qualifiedByName = "hashPassword")
+    @Mapping(target = "account", source = "accountId", qualifiedByName = "accountIdToAccount")
     User toUser(CreateUserRequest request);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "site", source = "siteId", qualifiedByName = "siteIdToSite")
     User updateUser(@MappingTarget User user, UpdateUserRequest request);
+
+    @Mapping(target = "account", source = "msaClient", qualifiedByName = "msaClientToAccount")
+    @Mapping(target = "role", defaultValue = "OTHER")
+    User ToUser(UserImportRequest request);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "account", source = "msaClient", qualifiedByName = "msaClientToAccount")
+    User updateUser(@MappingTarget User user, UserImportRequest request);
+
 }
