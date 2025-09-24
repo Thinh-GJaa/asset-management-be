1d18061b8e3d64c1e43846ed343041a28ab01ebb
Thinh_GJaa <xthinh4052002@gmail.com>
2025-09-24 11:49:03 +0700

    test send email

--- src/main/java/com/concentrix/asset/service/impl/EmailServiceImpl.java
+++ src/main/java/com/concentrix/asset/service/impl/EmailServiceImpl.java
@@ -41,6 +41,7 @@ public class EmailServiceImpl implements EmailService {
             MimeMessage message = mailSender.createMimeMessage();
             MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
 
+            helper.setFrom("itvn_noreply@concentrix.com");
             helper.setTo(to);
             helper.setSubject(subject);
             helper.setText(body, true);
