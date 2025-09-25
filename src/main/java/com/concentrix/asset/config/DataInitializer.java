8c9ec8fd361906889ff2bc529175b7cc7b28914a
Thinh-GJaa <xthinh04052002@gmail.com>
2025-09-26 00:11:58 +0700

    refactor config password in application.yml

--- src/main/java/com/concentrix/asset/config/DataInitializer.java
+++ src/main/java/com/concentrix/asset/config/DataInitializer.java
@@ -3,6 +3,7 @@ package com.concentrix.asset.config;
 import com.concentrix.asset.entity.User;
 import com.concentrix.asset.enums.Role;
 import com.concentrix.asset.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +11,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class DataInitializer {
+
+    @Value("${app.password.default-admin}")
+    String defaultPwdAdmin;
+
     @Bean
     CommandLineRunner initAdminUser(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         return args -> {
@@ -20,7 +25,7 @@ public class DataInitializer {
                         .fullName("Administrator")
                         .jobTitle("Administrator Asset Management System")
                         .email("admin_ams@concentrix.com")
-                        .password(passwordEncoder.encode("Adm1n@1234"))
+                        .password(passwordEncoder.encode(defaultPwdAdmin))
                         .role(Role.ADMIN)
                         .isActive(true)
                         .build();
