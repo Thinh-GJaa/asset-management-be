2e071aede4abf25854e70aeaeb0edaa8ed67d38b
Thinh_GJaa <xthinh4052002@gmail.com>
2025-10-14 11:09:48 +0700

    update SnapshotDevice ID

--- src/main/java/com/concentrix/asset/entity/SnapshotDeviceId.java
+++ src/main/java/com/concentrix/asset/entity/SnapshotDeviceId.java
@@ -6,11 +6,11 @@ import lombok.experimental.FieldDefaults;
 import java.io.Serializable;
 import java.time.LocalDate;
 
-@Data // gồm equals + hashCode + getter/setter/toString
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class SnapshotDeviceId implements Serializable {
-    Integer device;        // phải trùng tên với field trong entity
+    Integer device;
     LocalDate snapshotDate;
 }
